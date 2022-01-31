409de9f69102076da27edfeb2d1cced2146d587e
zerothreebot <bs03-rao-fbmi24@lll.kpi.ua>
2022-01-31 02:59:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -583,8 +583,8 @@ import schedule
 schedule.every(checkgmailevery).seconds.do(job)
 schedule.every().day.at("18:00").do(notifications_6hr_before)
 schedule.every().day.at("10:00").do(notifications_14hr_before)
-schedule.every().day.at("18:00").do(notifications_day_before)
-schedule.every().day.at("18:00").do(notifications_2days_before)
+schedule.every().day.at("19:00").do(notifications_day_before)
+schedule.every().day.at("20:00").do(notifications_2days_before)
 
 try:
     bot.send_message(393483876, '@rozklad_bot LOG: Bot started')
